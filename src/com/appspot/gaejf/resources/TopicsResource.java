c47c74f43942d4a9816dee75bbdef90613afa24f
mattkrae34 <mattkrae34@hotmail.com>
2010-12-06 19:06:46 -0500

    added resource logic
    Signed-off-by: mattkrae34 <mattkrae34@hotmail.com>

--- src/com/appspot/gaejf/resources/TopicsResource.java
+++ src/com/appspot/gaejf/resources/TopicsResource.java
@@ -1,18 +1,46 @@
 package com.appspot.gaejf.resources;
 
+import java.net.URI;
+
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
 
 @Path("/topics")
 public class TopicsResource {
+	
+	@Context
+	UriInfo uriInfo;
+	
+	@Context
+	Request request;
 
+	@Path("{topicId}")
+	public TopicResource topicResource(@PathParam("{topicId}") String topicId) {
+		return new TopicResource();
+	}
 	
 	@GET
 	@Produces (MediaType.APPLICATION_JSON)
 	public Response getTopics() {
 		return Response.ok("{\"hello\":\"world\"}").build();
+	}
+	
+	@POST
+	@Consumes (MediaType.APPLICATION_JSON)
+	public Response createTopic() {
+		UriBuilder ub = uriInfo.getAbsolutePathBuilder();
+		URI uri = ub.build("");
+		
+		return Response.created(uri).build();
 	}
 }
